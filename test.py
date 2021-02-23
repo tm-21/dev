b375c97022b66834acad15e82fda1ca9dd0d5bed
duncan <duncan@test.mail>
2021-02-24 01:14:09 +0900

    create class

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
-print("hello world")
 
-print("aa")
+class order():
+    def __init__(self):
+        super().__init__()
+        self.count = ""
+    def display(self,count,log):
+        print(count,log)
 
-
-import pandas
-
+N = 1
+OR1 = order()
+OR1.count = N
+OR2 = order()
+OR2.count = N+1
+OR1.display(OR1.count,"hello world")
+OR2.display(OR2.count,"aa")
